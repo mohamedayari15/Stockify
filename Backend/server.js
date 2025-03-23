7e4ac08eb15c8f6308a1b4c625a47e49608fe28b
Mohamed Ayari <mohamed.ayarii1507@gmail.com>
2025-03-23 08:27:04 +0100

    server update

--- Backend/server.js
+++ Backend/server.js
@@ -24,5 +24,5 @@ app.use('/uploads', express.static('uploads'));
 PortNumber = 3000 || process.env.PORT;
 
 app.listen(3000, () => {
-    console.log(`Serverr work on port ${PortNumber}`);
+    console.log(`Server work on port ${PortNumber}`);
 });
